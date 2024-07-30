45dbacee942a7f1995a2c770db4d6fdeebed5c31
uditangshu <145389897+uditangshu@users.noreply.github.com>
2024-07-31 01:05:42 +0530

    change

---
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -0,0 +1,73 @@
+import prisma from "@repo/db/client";
+import { AddMoney } from "../../../components/AddMoneyCard";
+import { BalanceCard } from "../../../components/BalanceCard";
+import { OnRampTransactions } from "../../../components/OnRampTransactions";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../lib/auth";
+import { stringify } from "querystring";
+
+async function getBalance() {
+    const session = await getServerSession(authOptions);
+    const balance = await prisma.balance.findFirst({
+        where: {
+            userId: Number(session?.user?.id)
+        }
+    });
+    return {
+        amount: balance?.amount || 0,
+        locked: balance?.locked || 0
+    }
+}
+
+async function getOnRampTransactions() {
+    const session = await getServerSession(authOptions);
+    const txns = await prisma.onRampTransaction.findMany({
+        where: {
+            userId: Number(session?.user?.id)
+        }
+    });
+    return txns.map(t => ({
+        time: t.startTime,
+        amount: t.amount,
+        status: t.status,
+        provider: t.provider
+    }))
+}
+async function getTransactionsP2P()
+{
+    const session = await getServerSession(authOptions);
+    const txns = await prisma.p2pTransfer.findMany({
+        where: {
+            fromUserId: Number(session?.user?.id)
+        }
+    });
+    return txns.map(t => ({
+        time: t.timestamp,
+        amount: t.amount,
+        status: t.fromUserId,
+        provider: t.timestamp
+    }))
+}
+
+export default async function() {
+    const balance = await getBalance();
+    const transactions = await getOnRampTransactions();
+    const transfer = await getTransactionsP2P()
+
+    return <div className="w-screen">
+        <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
+            Transfer
+        </div>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-4">
+            <div>
+                <AddMoney />
+            </div>
+            <div>
+                <BalanceCard amount={balance.amount} locked={balance.locked} />
+                <div className="pt-4">
+                    <OnRampTransactions transactions={transactions} />
+                </div>
+            </div>
+        </div>
+    </div>
+}
